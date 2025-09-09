5a2cf2787b9bdfbc00b164c396f681389690a998
shalanubale <ubaleshalan1836@gmail.com>
2025-09-09 12:42:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+    if (!str1 || !str2) {
+        return '';
+    }
+    return str1[0] + str2[0];
 }
 
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+// Examples
+console.log(shortcut("Amnesty", "International")); // 'AI'
+console.log(shortcut("Hello", "world"));           // 'Hw'
+console.log(shortcut("", "International"));        // ''
+console.log(shortcut("Amnesty", ""));              // ''
